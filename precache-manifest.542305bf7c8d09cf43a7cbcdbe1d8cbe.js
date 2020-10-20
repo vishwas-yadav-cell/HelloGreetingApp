9b3961de4724741773d93ca15e5631e97952f8ff
vishwas-yadav-cell <vishwasyadav164@gmail.com>
2020-10-20 16:19:15 +0530

    Updates

---
+++ precache-manifest.542305bf7c8d09cf43a7cbcdbe1d8cbe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0eca86e8edd3dfe81165a6cab6971493",
+    "url": "/HelloGreetingApp/index.html"
+  },
+  {
+    "revision": "a9d0f216a35ca46fd23f",
+    "url": "/HelloGreetingApp/static/css/main.8a2d5682.chunk.css"
+  },
+  {
+    "revision": "5171509348baf669b8a2",
+    "url": "/HelloGreetingApp/static/js/2.effcb247.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/HelloGreetingApp/static/js/2.effcb247.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9d0f216a35ca46fd23f",
+    "url": "/HelloGreetingApp/static/js/main.14a9ae0b.chunk.js"
+  },
+  {
+    "revision": "581e37a17cb31d8629d8",
+    "url": "/HelloGreetingApp/static/js/runtime-main.18b9ff8f.js"
+  }
+]);
